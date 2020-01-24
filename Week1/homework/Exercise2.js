18d7fb10297d8d5569e3f0d33e6f281bc4492228
karam awad <58743749+karam1984@users.noreply.github.com>
2020-01-24 19:35:40 +0100

    Update Exercise2.js

--- Week1/homework/Exercise2.js
+++ Week1/homework/Exercise2.js
@@ -1,5 +1,9 @@
 
-let karam ="I'm awesome";
+let karam ="I'm awesome"; // the propllem was the single quotation i just make it double quotation to solve it or i can use the backslash character to escape
+
 console.log(karam);
+
+
+
 
 
