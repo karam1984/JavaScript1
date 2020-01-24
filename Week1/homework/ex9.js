494a9c9ddd936ccd4be2b61aeaab2f497947526f
karam awad <58743749+karam1984@users.noreply.github.com>
2020-01-24 19:27:44 +0100

    Update ex9.js

--- Week1/homework/ex9.js
+++ Week1/homework/ex9.js
@@ -1,9 +1,9 @@
 let x = 7;
  x = x % 3;
-console.log(x)
+console.log(x) // x it will be 1 
 let y = 21;
 y = y % 4;
-console.log(y)
+console.log(y) // y it will be 1
 let z = 13;
 z = z %2;
-console.log(z)
+console.log(z) // z it will be 1
